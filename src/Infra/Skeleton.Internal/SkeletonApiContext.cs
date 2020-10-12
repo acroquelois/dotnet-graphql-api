db2e29e32741b8cd5d765199f1b6e22ca0542d0d
Croquelois <croquelois.adrien@gmail.com>
2020-10-12 16:44:01 +0200

    :building_construction: start architecture

---
+++ src/Infra/Skeleton.Internal/SkeletonApiContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skeleton.Internal
+{
+    public class SkeletonApiContext : DbContext
+    {
+        public SkeletonApiContext(DbContextOptions<SkeletonApiContext> options)
+            : base(options)
+        {
+        }
+    }
+}
